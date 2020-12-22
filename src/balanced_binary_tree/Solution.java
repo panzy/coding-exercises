bda8728e807c4889c03e4813f045dbddb6920fcf
Pan Zhiyong <panzhiyong84@gmail.com>
2020-12-22 11:53:16 -0500

    A simple solution to Balanced Binary Tree. Recursively get height of nodes.

---
+++ src/balanced_binary_tree/Solution.java
@@ -0,0 +1,53 @@
+/**
+ * Zhiyong Pan, 2020-12-22.
+ */
+package balanced_binary_tree;
+
+import java.util.LinkedList;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+
+        // scan the tree to check heights
+        boolean balanced = true;
+        LinkedList<TreeNode> layer = new LinkedList<>();
+        layer.add(root);
+        while (!layer.isEmpty()) {
+            TreeNode n = layer.removeFirst();
+            int leftHeight = getHeight(n.left);
+            int rightHeight = getHeight(n.right);
+            if (Math.abs(leftHeight - rightHeight) > 1) {
+                balanced = false;
+                break;
+            }
+            if (n.left != null) layer.add(n.left);
+            if (n.right != null) layer.add(n.right);
+        }
+
+        return balanced;
+    }
+
+    static int getHeight(TreeNode n) {
+        if (n == null) return 0;
+        return Math.max(
+                n.left == null ? 1 : getHeight(n.left) + 1,
+                n.right == null ? 1 : getHeight(n.right) + 1
+        );
+    }
+}
