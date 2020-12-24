cbfa85b9477ef8e9cd5145fa86f75e6db7dd8a4b
Pan Zhiyong <panzhiyong84@gmail.com>
2020-12-24 10:11:43 -0500

    Solved Swap Nodes in Pairs -- straightfoward linked list operations.

---
+++ src/swap_nodes_in_pairs/Solution.java
@@ -0,0 +1,55 @@
+/**
+ * Straightforward linked list operations, no complex algorithm involved.
+ *
+ * Zhiyong Pan, 2020-12-24
+ */
+package swap_nodes_in_pairs;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null)
+            return null;
+        if (head.next == null)
+            return head;
+
+        // this node will be the return value.
+        ListNode newHead = head.next;
+
+        ListNode prev = null;
+        ListNode A = head;
+        while (A != null) {
+            swap(A, prev);
+            prev = A;
+            A = A.next;
+        }
+
+        return newHead;
+    }
+
+    /**
+     * Swap ListNode A and its next.
+     * @param A
+     * @param prev the node before A.
+     */
+    static void swap(ListNode A, ListNode prev) {
+        if (A.next == null) return;
+
+        ListNode B = A.next;
+
+        if (prev != null)
+            prev.next = B;
+
+        A.next = B.next;
+        B.next = A;
+    }
+}
---
+++ src/swap_nodes_in_pairs/SolutionTest.java
@@ -0,0 +1,77 @@
+package swap_nodes_in_pairs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+    Solution s = new Solution();
+
+    @Test
+    void swap_noPrev() {
+        ListNode D = new ListNode(4);
+        ListNode C = new ListNode(3, D);
+        ListNode B = new ListNode(2, C);
+        ListNode A = new ListNode(1, B);
+
+        Solution.swap(A, null);
+        Assertions.assertEquals(A, B.next);
+        Assertions.assertEquals(C, A.next);
+        Assertions.assertEquals(D, C.next);
+    }
+
+    @Test
+    void swap_withPrev() {
+        ListNode D = new ListNode(4);
+        ListNode C = new ListNode(3, D);
+        ListNode B = new ListNode(2, C);
+        ListNode A = new ListNode(1, B);
+
+        Solution.swap(B, A);
+        Assertions.assertEquals(C, A.next);
+        Assertions.assertEquals(B, C.next);
+        Assertions.assertEquals(D, B.next);
+    }
+
+    @Test
+    void swapPairs() {
+        ListNode D = new ListNode(4);
+        ListNode C = new ListNode(3, D);
+        ListNode B = new ListNode(2, C);
+        ListNode A = new ListNode(1, B);
+
+        ListNode A1 = s.swapPairs(A);
+        Assertions.assertEquals(B, A1);
+        Assertions.assertEquals(A, B.next);
+        Assertions.assertEquals(D, A.next);
+        Assertions.assertEquals(C, D.next);
+        Assertions.assertEquals(null, C.next);
+    }
+
+    @Test
+    void swapPairs_empty() {
+        ListNode head = s.swapPairs(null);
+        Assertions.assertEquals(null, head);
+    }
+
+    @Test
+    void swapPairs_singleNode() {
+        ListNode A = new ListNode(1);
+
+        ListNode A1 = s.swapPairs(A);
+        Assertions.assertEquals(A, A1);
+        Assertions.assertEquals(null, A.next);
+    }
+
+    @Test
+    void swapPairs_oddNumbers() {
+        ListNode C = new ListNode(3, null);
+        ListNode B = new ListNode(2, C);
+        ListNode A = new ListNode(1, B);
+
+        ListNode A1 = s.swapPairs(A);
+        Assertions.assertEquals(B, A1);
+        Assertions.assertEquals(A, B.next);
+        Assertions.assertEquals(C, A.next);
+        Assertions.assertEquals(null, C.next);
+    }
+}
