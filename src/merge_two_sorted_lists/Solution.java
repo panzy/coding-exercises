4afa5ce181b72169c0b7cc6bc5c43b496c947fc6
Pan Zhiyong <panzhiyong84@gmail.com>
2021-01-04 12:42:45 -0500

    Used a dummy list head; appended the remained list as a whole if possible.

--- src/merge_two_sorted_lists/Solution.java
+++ src/merge_two_sorted_lists/Solution.java
@@ -9,34 +9,25 @@ import org.junit.jupiter.api.Test;
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = null, tail = null, p = null;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = dummyHead;
 
-        while (l1 != null || l2 != null) {
-            if (l1 == null) {
-                p = l2;
-                l2 = l2.next;
-            } else if (l2 == null) {
-                p = l1;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                p.next = l1;
                 l1 = l1.next;
             } else {
-                if (l1.val <= l2.val) {
-                    p = l1;
-                    l1 = l1.next;
-                } else {
-                    p = l2;
-                    l2 = l2.next;
-                }
+                p.next = l2;
+                l2 = l2.next;
             }
-
-            if (tail == null) {
-                head = tail = p;
-            } else {
-                tail.next = p;
-                tail = p;
-            }
+            p = p.next;
         }
 
-        return head;
+        // append the remained list
+        if (l1 != null) p.next = l1;
+        if (l2 != null) p.next = l2;
+
+        return dummyHead.next;
     }
 
     @Test
@@ -70,5 +61,21 @@ public class Solution {
         ListNode l2 = new ListNode(0);
         ListNode head = mergeTwoLists(l1, l2);
         Assertions.assertEquals(0, head.val);
+    }
+
+    @Test
+    void example4() {
+        ListNode l1 = new ListNode(1);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode head = mergeTwoLists(l1, l2);
+
+        Assertions.assertEquals(1, head.val);
+        Assertions.assertEquals(2, head.next.val);
+        Assertions.assertEquals(3, head.next.next.val);
+        Assertions.assertEquals(4, head.next.next.next.val);
     }
 }
