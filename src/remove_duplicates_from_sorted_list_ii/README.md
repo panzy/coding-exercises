2ba150f676b54f670397eb4bf5679cddeaeb4715
Pan Zhiyong <panzhiyong84@gmail.com>
2021-01-05 07:39:46 -0500

    Solved problem 82. Remove Duplicates from Sorted List II.

---
+++ src/remove_duplicates_from_sorted_list_ii/README.md
@@ -0,0 +1,9 @@
+# 82. Remove Duplicates from Sorted List II
+
+[[LeetCode page]](https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/)
+
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+## Notes
+
+It's not deduplication.
---
+++ src/remove_duplicates_from_sorted_list_ii/Solution.java
@@ -0,0 +1,72 @@
+package remove_duplicates_from_sorted_list_ii;
+
+import _lib.singly_linked_list.ListFactory;
+import _lib.singly_linked_list.ListNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Zhiyong Pan on 2021-01-05.
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = head, // the cursor
+
+                // nodes between [p,q) have the same value, i.e.,
+                // they are duplicates if there are more than one nodes in this range
+                q,
+
+                // the node before p
+                prev = null;
+
+        while (p != null) {
+            q = p.next;
+            if (q != null && q.val == p.val) {
+                while (q != null && q.val == p.val)
+                    q = q.next;
+                if (prev == null) {
+                    head = p = q;
+                } else {
+                    prev.next = p = q;
+                }
+            } else {
+                prev = p;
+                p = p.next;
+            }
+        }
+
+        return head;
+    }
+
+    @Test
+    void example1() {
+        ListNode head = ListFactory.fromArray(new int[]{1, 2, 3, 3, 4, 4, 5});
+        int[] output = ListFactory.toArray(deleteDuplicates(head));
+        int[] expected = {1, 2, 5};
+        Assertions.assertArrayEquals(expected, output);
+    }
+
+    @Test
+    void example2() {
+        ListNode head = ListFactory.fromArray(new int[]{1, 1, 1, 2, 3});
+        int[] output = ListFactory.toArray(deleteDuplicates(head));
+        int[] expected = {2, 3};
+        Assertions.assertArrayEquals(expected, output);
+    }
+
+    @Test
+    void example3() {
+        ListNode head = null;
+        int[] output = ListFactory.toArray(deleteDuplicates(head));
+        int[] expected = {};
+        Assertions.assertArrayEquals(expected, output);
+    }
+
+    @Test
+    void example4() {
+        ListNode head = new ListNode(1);
+        int[] output = ListFactory.toArray(deleteDuplicates(head));
+        int[] expected = {1};
+        Assertions.assertArrayEquals(expected, output);
+    }
+}
