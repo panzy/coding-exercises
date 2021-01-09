2792ca9793ee06fa97ce75ff3cc93742c217078f
Pan Zhiyong <panzhiyong84@gmail.com>
2021-01-08 21:35:27 -0500

    Solving problem Wildcard Matching.

---
+++ src/wildcard_matching/Solution.java
@@ -0,0 +1,207 @@
+package wildcard_matching;
+
+import java.util.ArrayList;
+
+/**
+ * Adapted from {@link regular_expression_matching.Solution}.
+ *
+ * Created by Zhiyong Pan on 2021-01-08.
+ */
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        Token[] tokens = parsePattern(p);
+        return isMatch(s, tokens);
+    }
+
+    public boolean isMatch(String s, Token[] tokens) {
+        if (tokens.length == 0)
+            return s.isEmpty();
+
+        if (!tokens[0].anyTimes) {
+            return s.length() > 0 &&
+                    tokens[0].match(s.charAt(0)) &&
+                    isMatch(s.substring(1),
+                            copyTokensWithoutState(tokens, 1, tokens.length));
+        }
+
+        if (!tokens[tokens.length - 1].anyTimes) {
+            return s.length() > 0 &&
+                    tokens[tokens.length - 1].match(s.charAt(s.length() - 1)) &&
+                    isMatch(s.substring(0, s.length() - 1),
+                            copyTokensWithoutState(tokens, 0, tokens.length - 1));
+        }
+
+        int ti = -1;
+
+        // find the first plain text token
+        for (int i = 0; i < tokens.length; ++i) {
+            if (!tokens[i].anyTimes && tokens[i].target != '?') {
+                if (ti == -1) {
+                    ti = i;
+                    break;
+                }
+            }
+        }
+
+        // if there is a plain text token, use it to divide and conquer.
+        // notice that there might be multiple dividing point in the string, in that case,
+        // we try them all until a successful match is found.
+        if (ti != -1) {
+            int i = s.indexOf(tokens[ti].target);
+            if (i == -1) {
+                return false;
+            } else {
+                boolean found = false;
+                do {
+                    if (splitAndMatch(s, tokens, ti, i)) {
+                        found = true;
+                        break;
+                    }
+
+                    i = s.indexOf(tokens[ti].target, i + 1);
+                } while (i != -1);
+
+                return found;
+            }
+        }
+
+        ti = 0;
+        int i = 0;
+        while (i < s.length() && ti < tokens.length) {
+            Token token = tokens[ti];
+            char c = s.charAt(i);
+
+            if (token.match(c)) {
+                ++i;
+                if (!token.anyTimes) {
+                    ++ti;
+                }
+            } else if (token.anyTimes) {
+                do {
+                    ++ti;
+
+                    // let the prev token eat this one
+                    if (ti < tokens.length && tokens[ti].target != '?'
+                            && tokens[ti].target == tokens[ti - 1].target && tokens[ti - 1].anyTimes)
+                        continue;
+                    else
+                        break;
+                } while (true);
+            } else if (ti > 0 && tokens[ti - 1].target == c && tokens[ti - 1].anyTimes) {
+                // drop this token (and don't touch the previous token)
+                ++ti;
+            } else {
+                return false;
+            }
+        }
+
+        if (i < s.length())
+            return false;
+
+        while (ti < tokens.length) {
+            Token t = tokens[ti];
+            if (t.actualTimes < t.minTimes) {
+                // it's still fine if there is a previous token with a compliant target having extra actual match times.
+
+                int tj = ti - 1;
+                while (tj >= 0 &&
+                        !(
+                                (tokens[tj].target == t.target || tokens[tj].target == '?' || t.target == '?') &&
+                                        tokens[tj].actualTimes > tokens[tj].minTimes
+                        )
+                ) {
+                    --tj;
+                }
+
+                if (tj < 0)
+                    return false;
+
+                // move the match from [tj] to [ti].
+                --tokens[tj].actualTimes;
+            }
+            ++ti;
+        }
+
+        return true;
+    }
+
+    private Token[] copyTokensWithoutState(Token[] tokens, int begin, int end) {
+        Token[] a = new Token[end - begin];
+        for (int i = begin; i < end; ++i) {
+            a[i - begin] = new Token(tokens[i].target);
+        }
+        return a;
+    }
+
+    private boolean splitAndMatch(String s, Token[] tokens, int ti, int i) {
+        if (!isMatch(s.substring(0, i), copyTokensWithoutState(tokens, 0, ti)))
+            return false;
+
+        if (!isMatch(s.substring(i + 1), copyTokensWithoutState(tokens, ti + 1, tokens.length)))
+            return false;
+
+        return true;
+    }
+
+    static Token[] parsePattern(String p) {
+        ArrayList<Token> tokens = new ArrayList<>();
+        for (int i = 0; i < p.length(); ++i) {
+            if (i > 0 && p.charAt(i) == '*' && p.charAt(i - 1) == '*')
+                continue;
+            Token t = new Token(p.charAt(i));
+            t.position = i;
+            tokens.add(t);
+        }
+        return tokens.toArray(new Token[tokens.size()]);
+    }
+
+    /**
+     * A token of a pattern. It can match a char various times.
+     */
+    static class Token {
+        /**
+         * The char to match. '?' means any char.
+         */
+        char target;
+        /**
+         * Does this token match any (including zero) times or exactly one time?
+         */
+        boolean anyTimes;
+
+        /**
+         * Actual matched times.
+         */
+        int actualTimes;
+
+        int minTimes;
+
+        /**
+         * Position in the regexp. This is for debug purpose.
+         */
+        int position;
+
+        public Token(char target) {
+            this.target = target;
+            this.anyTimes = target == '*';
+            actualTimes = 0;
+            minTimes = anyTimes ? 0 : 1;
+        }
+
+        /**
+         * Test whether the given char matches this token or not.
+         */
+        public boolean match(char c) {
+            if (this.target == c || this.target == '?' || this.target == '*') {
+                ++actualTimes;
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "" + target;
+        }
+    }
+}
---
+++ src/wildcard_matching/WildcardMatching.java
@@ -0,0 +1,162 @@
+package wildcard_matching;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Zhiyong Pan on 2021-01-08.
+ */
+public class WildcardMatching {
+    Solution solution = new Solution();
+
+    @Test
+    void example1() {
+        Assertions.assertFalse(solution.isMatch("aa", "a"));
+    }
+
+    @Test
+    void example2() {
+        Assertions.assertTrue(solution.isMatch("aa", "*"));
+    }
+
+    @Test
+    void example3() {
+        Assertions.assertFalse(solution.isMatch("cb", "?a"));
+    }
+
+    @Test
+    void example4() {
+        Assertions.assertTrue(solution.isMatch("adceb", "*a*b"));
+    }
+
+    @Test
+    void example5() {
+        Assertions.assertFalse(solution.isMatch("acdcb", "a*c?b"));
+    }
+
+    @Test
+    void example5_A() {
+        Assertions.assertTrue(solution.isMatch("aaaaab", "a*ab"));
+    }
+    @Test
+    void example5_B() {
+        Assertions.assertTrue(solution.isMatch("aaaaab", "a*aab"));
+    }
+    @Test
+    void example5_C() {
+        Assertions.assertTrue(solution.isMatch("aaaaab", "a*aaaab"));
+    }
+    @Test
+    void example5_D() {
+        Assertions.assertFalse(solution.isMatch("aaaaab", "a*aaaaab"));
+    }
+    @Test
+    void example5_E() {
+        Assertions.assertTrue(solution.isMatch("aaabc!", "a*aabc!"));
+    }
+    @Test
+    void example5_F() {
+        Assertions.assertTrue(solution.isMatch("aaaaa", "a*a"));
+    }
+    @Test
+    void example5_G() {
+        Assertions.assertTrue(solution.isMatch("aaabc!", "a*abc!"));
+    }
+
+    @Test
+    void example6() {
+        Assertions.assertTrue(solution.isMatch("aaabc!", "*a*abc!"));
+    }
+
+    @Test
+    void example7() {
+        Assertions.assertTrue(solution.isMatch("aaabc!", "a**!"));
+    }
+
+    @Test
+    void example8() {
+        Assertions.assertTrue(solution.isMatch("", ""));
+    }
+
+    @Test
+    void example9() {
+        Assertions.assertTrue(solution.isMatch("", "*"));
+    }
+
+    @Test
+    void example10() {
+        Assertions.assertTrue(solution.isMatch("", "**"));
+    }
+
+    @Test
+    void example11() {
+        Assertions.assertTrue(solution.isMatch("abc", "a*b*****"));
+    }
+
+    @Test
+    void example12() {
+        Assertions.assertTrue(solution.isMatch("abcxyz", "a*b*????"));
+    }
+
+    @Test
+    void example13() {
+        Assertions.assertTrue(solution.isMatch("abcxyz", "a*b*?*???"));
+    }
+
+    @Test
+    void example15() {
+        Assertions.assertTrue(solution.isMatch("ab!@#", "a??**#"));
+    }
+
+    @Test
+    void example16() {
+        Assertions.assertTrue(solution.isMatch("ab!@#y#", "a*b*?*?*??#"));
+    }
+
+    @Test
+    void example17() {
+        Assertions.assertTrue(solution.isMatch("ab", "?*"));
+    }
+
+    @Test
+    void example18() {
+        Assertions.assertFalse(solution.isMatch("aaa", "aaaa"));
+    }
+
+    @Test
+    void example19() {
+        Assertions.assertTrue(solution.isMatch("a", "a*"));
+    }
+
+    @Test
+    void example20() {
+        Assertions.assertTrue(solution.isMatch("a", "*a*"));
+    }
+
+    @Test
+    void example21() {
+        Assertions.assertTrue(solution.isMatch("bbbba", "**a"));
+    }
+
+    @Test
+    void example22() {
+        Assertions.assertTrue(solution.isMatch("ab", "*??*"));
+    }
+
+    @Test
+    void example23() {
+        Assertions.assertTrue(solution.isMatch("aaa", "a*a**a"));
+    }
+
+    @Test
+    void example24() {
+        Assertions.assertTrue(solution.isMatch("aasdfasdfasdfasdfas", "aasdf*asdf*asdf*asdf*s"));
+    }
+
+    @Test
+    void test1710() {
+        String s = "abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb";
+        String p = "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb";
+//        Assertions.assertTrue(solution.isMatch(s, p));
+    }
+}
