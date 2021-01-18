1e473e6cc34a725b9403012524419e51af4c355b
Pan Zhiyong <panzhiyong84@gmail.com>
2021-01-18 15:24:47 -0500

    Optimization: used an user-defined data type to reduce hash looking times.

--- src/possible_bipartition_886/Solution2.java
+++ src/possible_bipartition_886/Solution2.java
@@ -1,7 +1,5 @@
 package possible_bipartition_886;
 
-import _lib.graph.Graphs;
-
 import java.util.*;
 
 /**
@@ -11,20 +9,32 @@ import java.util.*;
  * Created by Zhiyong Pan on 2021-01-18.
  */
 public class Solution2 {
+    static class Node {
+        /**
+         * Neighbours' indices.
+         */
+        List<Integer> neighbours = new LinkedList<>();
+
+        /**
+         * -1 = Uncolored.
+         * 0 = The 1st color.
+         * 1 = The 2nd color.
+         */
+        int color = -1;
+        /**
+         * For traverse.
+         */
+        boolean visited = false;
+    }
+
     int N;
 
     // key = a person's index (1-based)
-    // value = that person's dislikes
-    HashMap<Integer, List<Integer>> edges;
-
-    // key = a person's index (1-based)
-    // value = that person's group color (0 or 1).
-    HashMap<Integer, Integer> colors;
+    HashMap<Integer, Node> graph;
 
     public boolean possibleBipartition(int N, int[][] dislikes) {
         this.N = N;
-        edges = Graphs.buildFromEdges(dislikes);
-        colors = new HashMap<>(1 + N);
+        graph = buildFromEdges(dislikes);
 
         for (int i = 1; i <= N; ++i) {
             if (!bipartition(i))
@@ -38,43 +48,68 @@ public class Solution2 {
      * Bi-partition a whole isolated graph component.
      */
     boolean bipartition(int seed) {
-        if (colors.containsKey(seed)) // already assigned
+        Node seedNode = graph.get(seed);
+
+        if (seedNode == null) // This person is an isolate node, he can go to either group.
             return true;
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        BitSet bfsVisited = new BitSet(1 + N);
+        // If this person is colored, then the whole connected graph component he belongs to should have also been colored.
+        if (seedNode.color != -1)
+            return true;
+
+        Queue<Node> queue = new ArrayDeque<>();
 
         // Since seed is the first person in his isolated graph component,
         // either group is ok for him. We choose group A.
-        queue.add(seed);
-        bfsVisited.set(seed);
-        colors.put(seed, 0);
+        queue.add(seedNode);
+        seedNode.visited = true;
+        seedNode.color = 0;
 
         boolean failed = false;
 
         while (!failed && !queue.isEmpty()) {
-            int i = queue.poll();
-            int g = colors.get(i);
-            List<Integer> neighbours = edges.get(i);
-            if (neighbours == null)
-                continue;
-            for (int j : neighbours) {
-                if (!colors.containsKey(j)) {
+            Node i = queue.poll();
+            for (int j2 : i.neighbours) {
+                Node j = graph.get(j2);
+                if (j.color == -1) {
                     // Put j to the other side.
-                    colors.put(j, 1 - g);
-                } else if (colors.get(j) == g) {
+                    j.color = 1 - i.color;
+                } else if (j.color == i.color) {
                     // Conflict detected.
                     failed = true;
                     break;
                 }
 
-                if (!bfsVisited.get(j)) {
-                    bfsVisited.set(j);
+                if (!j.visited) {
+                    j.visited = true;
                     queue.add(j);
                 }
             }
         }
 
         return !failed;
+    }
+
+    static HashMap<Integer, Node> buildFromEdges(int[][] edges) {
+        HashMap<Integer, Node> graph = new HashMap<>();
+
+        // collect graph graph
+        for (int[] pair : edges) {
+            // Append j to i's list.
+            {
+                Node a = graph.getOrDefault(pair[0], new Node());
+                a.neighbours.add(pair[1]);
+                graph.put(pair[0], a);
+            }
+
+            // Append i to j's list.
+            {
+                Node b = graph.getOrDefault(pair[1], new Node());
+                b.neighbours.add(pair[0]);
+                graph.put(pair[1], b);
+            }
+        }
+
+        return graph;
     }
 }
