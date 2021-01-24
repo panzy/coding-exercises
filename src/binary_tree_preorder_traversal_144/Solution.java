8efa6360c9197ad148f140ac60650805dc90a394
Pan Zhiyong <panzhiyong84@gmail.com>
2021-01-24 10:06:16 -0500

    Solved problem 144. Binary Tree Preorder Traversal.

---
+++ src/binary_tree_preorder_traversal_144/Solution.java
@@ -0,0 +1,46 @@
+package binary_tree_preorder_traversal_144;
+
+import _lib.bintree.TreeFactory;
+import _lib.bintree.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Zhiyong Pan on 2021-01-24.
+ */
+public class Solution {
+    List<Integer> ans;
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ans = new ArrayList<>();
+        if (root != null)
+            preorder(root);
+        return ans;
+    }
+
+    private void preorder(TreeNode root) {
+        assert root != null;
+        ans.add(root.val);
+        if (root.left != null)
+            preorder(root.left);
+        if (root.right != null)
+            preorder(root.right);
+    }
+
+    @Test
+    void testPreorder() {
+        Assertions.assertArrayEquals(new Integer[]{}, preorderTraversal(TreeFactory.fromArray(new Integer[]{
+        })).toArray(new Integer[0]));
+
+        Assertions.assertArrayEquals(new Integer[]{1}, preorderTraversal(TreeFactory.fromArray(new Integer[]{
+                1
+        })).toArray(new Integer[1]));
+
+        Assertions.assertArrayEquals(new Integer[]{1, 2, 3}, preorderTraversal(TreeFactory.fromArray(new Integer[]{
+                1, null, 2, 3
+        })).toArray(new Integer[3]));
+    }
+}
