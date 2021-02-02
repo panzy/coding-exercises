2ec0a26cdb7d935d70bfad4b1af4c39559a27c88
Pan Zhiyong <panzhiyong84@gmail.com>
2021-02-02 07:58:36 -0500

    Solved problem 669. Trim a Binary Search Tree.

---
+++ src/trim_a_binary_search_tree_669/Solution.java
@@ -0,0 +1,22 @@
+package trim_a_binary_search_tree_669;
+
+import _lib.bintree.TreeNode;
+
+/**
+ * Created by Zhiyong Pan on 2021-02-02.
+ */
+public class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        } else if (low <= root.val && root.val <= high) {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+            return root;
+        } else if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else {
+            return trimBST(root.left, low, high);
+        }
+    }
+}
