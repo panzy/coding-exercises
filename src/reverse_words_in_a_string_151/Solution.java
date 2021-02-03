b12d23b1d47d1253c4dab820c8dd0fd5fa912a36
Pan Zhiyong <panzhiyong84@gmail.com>
2021-02-03 14:09:06 -0500

    Solved problem 151. Reverse Words in a String.

---
+++ src/reverse_words_in_a_string_151/Solution.java
@@ -0,0 +1,44 @@
+package reverse_words_in_a_string_151;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by Zhiyong Pan on 2021-02-03.
+ */
+class Solution {
+    public String reverseWords(String s) {
+        // Prepending a sentinel white space will make the program much simpler.
+        char[] A = (' ' + s).toCharArray();
+        int n = A.length;
+        StringBuilder sb = new StringBuilder(n);
+        int wend = n;
+        boolean added = false;
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (A[i] == ' ') {
+                if (i + 1 < wend) {
+                    if (added) {
+                        sb.append(' ');
+                    } else {
+                        added = true;
+                    }
+                    sb.append(Arrays.copyOfRange(A, i + 1, wend));
+                }
+                wend = i;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    @Test
+    void examples() {
+        Assertions.assertEquals("blue is sky the", reverseWords("the sky is blue"));
+        Assertions.assertEquals("example good a", reverseWords("a good   example"));
+        Assertions.assertEquals("Alice Loves Bob", reverseWords("  Bob    Loves  Alice   "));
+        Assertions.assertEquals("bob like even not does Alice", reverseWords("Alice does not even like bob"));
+    }
+}
