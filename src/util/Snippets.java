a6e6a829d174b2383c7df0e21d3ef9d3145827ba
Pan Zhiyong <panzhiyong84@gmail.com>
2021-02-07 09:10:46 -0500

    new file:   Snippets.java

---
+++ src/util/Snippets.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.util.stream.IntStream;
+
+/**
+ * Created by Zhiyong Pan on 2021-02-07.
+ */
+public class Snippets {
+    void streamWithIndices() {
+        char[] A = "abcdefg".toCharArray();
+        int n = A.length;
+        int[] anchors = IntStream.range(0, n).filter(i -> A[i] == 'c').toArray();
+    }
+}
