6e568dc23ad119db62b04afc5e7826518bf03a01
Pan Zhiyong <panzhiyong84@gmail.com>
2021-02-26 10:25:11 -0500

    Added between() and dist2() functions.

--- util.cpp
+++ util.cpp
@@ -28,3 +28,11 @@ ULL permutation(int n, int r)
 	}
 	return ans;
 }
+
+bool between(int x, int a, int b) {
+    return a <= x && x <= b;
+}
+
+int dist2(int x1, int y1, int x2, int y2) {
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+}
