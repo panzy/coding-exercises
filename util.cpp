bc4380d70e6d8afa04cd7da51e79264b3af9dade
Pan Zhiyong <panzhiyong84@gmail.com>
2021-02-28 14:24:18 -0500

    Moved some code snippets from Solution.cpp to util.cpp.

--- util.cpp
+++ util.cpp
@@ -1,6 +1,25 @@
 typedef long long LL;
 typedef unsigned long long ULL;
 
+#define FOR(i, n) for (int i = 0; i < n; ++i)
+#define FOR_RANGE(i, begin, end) for (int i = begin; i < end; ++i)
+
+// 4 neighbour cells in a grid.
+const int NEIBS4[4][2] = { {-1, 0}, {0, -1}, {1, 0}, {0, 1} };
+
+template <class T>
+pair<int, int> grid_size(const vector<vector<T>>& grid) {
+    return { grid.size(), grid[0].size() };
+}
+
+template <class T>
+T min_element_2d(const vector<vector<T>>& grid) {
+    T ans = numeric_limits<T>::max();
+    for (auto&& row : grid)
+        ans = min(ans, *min_element(row.begin(), row.end()));
+    return ans;
+}
+
 /*
 https://stackoverflow.com/a/9331125/1350949 
 Tips: Excel COMBIN() function can be used to verify this.
