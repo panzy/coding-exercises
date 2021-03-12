19653e516f65f66570c171444e04708ab485115b
Pan Zhiyong <panzhiyong84@gmail.com>
2021-03-12 10:24:41 -0500

    Added a slightly different implementation.

--- archives/check_if_a_string_contains_all_binary_codes_of_size_k_1461/Solution.cpp
+++ archives/check_if_a_string_contains_all_binary_codes_of_size_k_1461/Solution.cpp
@@ -24,7 +24,14 @@ Zhiyong
 #include <bitset>
 using namespace std;
 
-class Solution {
+// time: beats 94.61%
+// memory: beats 96.49%
+//
+// If track the count of distinct numbers, even though in the end we don't have to do
+// a O(2^k) traveral on the bitset, the overall time is slightly longer. I think that's
+// because to maintain the distinct count, we have to query the bitset each time to see
+// whether a number is appear for the first time.
+class Solution1 {
 public:
     bool hasAllCodes(string s, int k) {
         bitset<1048576> nums; // 2^20 = 1048576
@@ -56,10 +63,47 @@ public:
     }
 };
 
+// Same idea, slightly different implementation:
+// 1. replaced bitset with bool array on heap;
+// 2. count distinct numbers on the fly.
+//
+// Runtime: 52 ms, faster than 94.61% of C++ online submissions for Check If a String Contains All Binary Codes of Size K.
+// Memory Usage : 27.1 MB, less than 80.33 % of C++ online submissions for Check If a String Contains All Binary Codes of Size K.
+class Solution2 {
+public:
+    bool hasAllCodes(string s, int k) {
+        size_t endNum = (size_t)pow(2, k);
+        unique_ptr<bool[]> nums(new bool[endNum]);
+        fill(nums.get(), nums.get() + endNum, false);
+
+        int num = 0;
+        int n = s.length();
+        int cnt = 0;
+
+        // For each substring of length k, record its binary value in the bitset.
+        for (int i = 0; i < n; ++i) {
+            num <<= 1;
+            num += s[i] - '0';
+
+            if (i >= k) {
+                num &= ~(1 << k); // drop the leftmost bit
+            }
+
+            if (i + 1 >= k) {
+                if (!nums[num])
+                    ++cnt;
+                nums[num] = true;
+            }
+        }
+
+        return cnt == endNum;
+    }
+};
+
 int main() {
     int ans;
     vector<int> coins;
-    //using Solution = Solution4;
+    using Solution = Solution2;
 
     assert(Solution().hasAllCodes("01", 1));
     assert(Solution().hasAllCodes("10", 1));
