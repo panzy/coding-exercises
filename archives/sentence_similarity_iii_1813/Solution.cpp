e1294cda5ea134890fe28d40a2f34b2ab9acf247
Pan Zhiyong <panzhiyong84@gmail.com>
2021-04-04 00:31:55 -0400

    Simplified a bit.

--- archives/sentence_similarity_iii_1813/Solution.cpp
+++ archives/sentence_similarity_iii_1813/Solution.cpp
@@ -15,43 +15,30 @@
 class Solution {
 public:
     bool areSentencesSimilar(string s1, string s2) {
-        cout << s1 << endl << s2 << endl;
         int m = s1.length();
         int n = s2.length();
-        int i = 0, // length of head
-        j = 0; // length of tail
-        
-        while (i < m && i < n && s1[i] == s2[i]) ++i;
-        
-        cout << "i=" << i << endl;
-        if (i > 0) {
-            // back to the nearest space
-            if (i < m)
-                while (i > 0 && s1[i] != ' ') --i;
-            if (i < n)
-                while (i > 0 && s2[i] != ' ') --i;
+        int i = 0, // length of head (not including the OPTIONAL ending whitespace)
+        j = 0; // length of tail (not including the OPTIONAL ending whitespace)
+
+        int k = 0;
+        while (k < m && k < n && s1[k] == s2[k]) {
+            if (s1[k] == ' ')
+                i = k; // the latest whitespace
+            ++k;
         }
-        cout << "i=" << i << endl;
-        
-        while (j < m - i && j < n - i && s1[m - j - 1] == s2[n - j - 1]) ++j;
-        
-        cout << "j=" << j << endl;
-        if (j > 0 && m - j > 0 && (n - j != 0 && s1[m - j] != ' ')) {
-            cout << "break word 1" << endl;
-            return false;
+        // exhausted?
+        if ((k == m || s1[k] == ' ') && (k == n || s2[k] == ' ')) i = k;
+
+        k = 0;
+        while (k < m - i && k < n - i && s1[m - 1 - k] == s2[n - 1 - k]) {
+            if (s1[m - 1 - k] == ' ')
+                j = k; // the latest whitespace
+            ++k;
         }
-        if (j > 0 && n - j > 0 && (m - j != 0 && s2[n - j] != ' ')) {
-            cout << "break word 2" << endl;
-            return false;
-        }
-        if (s1[m - j] == ' ') {
-            --j;
-        }
-        cout << "j=" << j << endl;
-        
+        // exhausted?
+        if (k == m || k == n) j = k;
+
         int len = i + j + (i > 0 && j > 0 ? 1 : 0);
-        bool ans = len == m || len == n;
-        cout << (ans ? "true" : "false") << endl;
-        return ans;
+        return len == m || len == n;
     }
 };
