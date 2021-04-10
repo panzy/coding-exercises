5d4fc694e54d1c50102b005974fa87c6b9bb3b96
Pan Zhiyong <panzhiyong84@gmail.com>
2021-04-10 15:53:45 -0400

    Minor optimizations.

--- archives/k_concatenation_maximum_sum_1191/Solution.cpp
+++ archives/k_concatenation_maximum_sum_1191/Solution.cpp
@@ -13,20 +13,16 @@ public:
         // M: maximal sum of subarray [i:j]
         // L0: minimal sum of subarray [0:i]
         long long F = 0, L = 0, L0 = 0, M = 0, R = 0;
-        
+
         for (int e : arr) {
             F += e;
             if (L0 > F) L0 = F;
             if (L < F) L = F;
             if (M < F - L0) M = F - L0;
         }
-        
-        F = 0;
-        for (int i = arr.size() - 1; i >= 0; --i) {
-            F += arr[i];
-            if (R < F) R = F;
-        }
-        
+
+        R = F - L0;
+
         long long ans = M;
         if (F > 0)
             ans = max(ans, F * k);
@@ -34,6 +30,6 @@ public:
             ans = max(ans, max(R, L));
         else
             ans = max(ans, R + (F > 0 ? F * (k - 2) : 0) + L);
-        return ans % (long long)(1e9 + 7);
+        return ans % 1000000007;
     }
 };
